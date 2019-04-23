3b280c524a5d2decb2d161fbc512a508343a8837
hhwkns <hhwkns@gmail.com>
2019-04-22 20:48:48 -0700

    l3

---
+++ lesson03/assignment/data.js
@@ -0,0 +1,21 @@
+  $(function () {
+  let dataHTML = data.map(function(i) {
+    let data = `<section class="left">
+      <img src="img/unsplash-headshot.jpg" alt="steve-Smith">
+      <h1 class = "data__name">${i.name}</h1>
+      <p class ="data__jobTitle">${i.jobTitle}</p>
+    </section>
+    <section class="right">
+      <p class="data__company"><strong>${i.company}</strong></p>
+      <p class="data__experience"><strong>${i.experience}</strong></p>
+      <p class="data__school"><strong>${i.school}</strong></p>
+      <p class="data__major"><strong>${i.major}</strong></p>
+      <p class="data__email"><strong>${i.email}</strong></p>
+      <i class="fab fa-linkedin fa-2x"></i>
+      <p class="data_LinkedInUrl">${i.LinkedInUrl}</p>
+    </section>`;
+  
+
+  $(".data").append(dataHTML);
+  });
+});
