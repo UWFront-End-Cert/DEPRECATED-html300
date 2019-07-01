cb3ad926f1c26f88b4e30f39b5cbd5e494f56572
georgeme <georgeme@uw.edu>
2019-07-01 15:31:37 -0700

    Adding HTML and JS code for more functions

--- lesson01-georgeme/scripts.js
+++ lesson01-georgeme/scripts.js
@@ -1,22 +1,53 @@
+// Function for Reduce
 $(function () {
 // Creating variables with data values
     const numbers = [16, 32, 64, 128],
-        inputConst = $('.reduceInput'),
-        submitConst = $('.reduceSubmit'),
+        inputConstRed = $('.reduceInput'),
+        submitConstRed = $('.reduceSubmit'),
         resultSum = $('.sum');
 // Event when clicking submit
-      submitConst.on('click', function () {
+      submitConstRed.on('click', function () {
 // ParseInt takes the string value and makes it a number
-        const val = inputConst.val();
+        const val = inputConstRed.val();
         const num = parseInt(val);
 // Checking if a number is entered and if so, adding to the total of the array numbers (using push)
         if (!isNaN(num)) {
           numbers.push(num);
 // "Reducing" the entire total of the numbers in the array + the user entered number to one value
-          const sum = numbers.reduce((arr, user) => arr + user);
+          const sumReduce = numbers.reduce((arr, user) => arr + user);
 // Adding the new total value (sum) to the text on the HTML page
-          resultSum.text(sum);
-          input.val('');
+          resultSum.text(sumReduce);
+          input.valReduce('');
+// Creating an alert if a NaN is entered
+        } else {
+          alert("Not a number! You must enter only numeric values.");
+        }
+    });
+});
+
+
+// Function for Every
+$(function () {
+// Creating variables with data values
+    const numbers = [77, 89, 101, 250],
+        inputConstEv = $('.everyInput'),
+        submitConstEv = $('.everySubmit'),
+        resultEv = $('.every');
+// Event when clicking submit
+      submitConstEv.on('click', function () {
+// ParseInt takes the string value and makes it a number
+        const val = inputConstEv.val();
+        const num = parseInt(val);
+// Checking if a number is entered and if so, adding to the total of the array numbers (using push)
+        if (!isNaN(num)) {
+          numbers.push(num);
+// "Reducing" the entire total of the numbers in the array + the user entered number to one value
+          const listEvery = numbers.every(function(val) {
+            return val > 62;
+          });
+// Adding the new total value (sum) to the text on the HTML page
+          resultEv.text(listEvery);
+          input.valReduce('');
 // Creating an alert if a NaN is entered
         } else {
           alert("Not a number! You must enter only numeric values.");
