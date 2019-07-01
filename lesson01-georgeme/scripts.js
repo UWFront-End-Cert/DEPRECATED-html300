e4a9fec9de089b6c73bde5b13a4ee65dbddb3d5f
georgeme <georgeme@uw.edu>
2019-07-01 12:48:02 -0700

    Added code for first user input

--- lesson01-georgeme/scripts.js
+++ lesson01-georgeme/scripts.js
@@ -1,20 +1,25 @@
-// This is the array and code to determine if one of the magic numbers is selected
-function magicNumber() {
-let numbers = [1, 3, 5, 7, 9, 11, 13];
-  input = $('.magicNum');
-  if input == numbers {
-    alert('You win!');
-  }
-};
-
-
-
-
-// PI
-function pi() {
-const pi = "3.14159";
-if {
-
-}
-alert(hello.includes("wri"));
-}
+$(function () {
+// Creating variables with data values
+    const numbers = [16, 32, 64, 128],
+        inputConst = $('.reduceInput'),
+        submitConst = $('.reduceSubmit'),
+        resultSum = $('.sum');
+// Event when clicking submit
+      submitConst.on('click', function () {
+// ParseInt takes the string value and makes it a number
+        const val = inputConst.val();
+        const num = parseInt(val);
+// Checking if a number is entered and if so, adding to the total of the array numbers (using push)
+        if (!isNaN(num)) {
+          numbers.push(num);
+// "Reducing" the entire total of the numbers in the array + the user entered number to one value
+          const sum = numbers.reduce((arr, user) => arr + user);
+// Adding the new total value (sum) to the text on the HTML page
+          resultSum.text(sum);
+          input.val('');
+// Creating an alert if a NaN is entered
+        } else {
+          alert("Not a number! You must enter only numeric values.");
+        }
+    });
+});
