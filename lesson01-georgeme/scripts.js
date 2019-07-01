742e83d798edda0803f4def679dc42c2efafa827
georgeme <georgeme@uw.edu>
2019-07-01 07:40:03 -0700

    Added code to all pages

---
+++ lesson01-georgeme/scripts.js
@@ -0,0 +1,20 @@
+// This is the array and code to determine if one of the magic numbers is selected
+function magicNumber() {
+let numbers = [1, 3, 5, 7, 9, 11, 13];
+  input = $('.magicNum');
+  if input == numbers {
+    alert('You win!');
+  }
+};
+
+
+
+
+// PI
+function pi() {
+const pi = "3.14159";
+if {
+
+}
+alert(hello.includes("wri"));
+}
