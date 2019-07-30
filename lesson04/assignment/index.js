f14cf388e5e7db3b441293ab570187ea611e8ff1
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-07-30 11:03:25 -0700

    Update index.js

--- lesson04/assignment/index.js
+++ lesson04/assignment/index.js
@@ -1,5 +1,5 @@
 // JavaScript source code
-var images = ["vsd.jpg", "fdsdb.jpg", "MV5BMjAzNjYwNzkxMl5BMl5BanBnXkFtZTgwMTU1OTk4MDE@._V1_UY317_CR16,0,214,317_AL_.jpg", "benjamin-parker-OhKElOkQ3R-unsplash-cropped.jpg"];
+var images = ["img/vsd.jpg", "img/fdsdb.jpg", "img/MV5BMjAzNjYwNzkxMl5BMl5BanBnXkFtZTgwMTU1OTk4MDE_V1_UY317_CR16,0,214,317_AL_.jpg", "img/benjamin-parker-OhKElOkQ3R-unsplash-cropped.jpg"];
 var i = 0;
 var p = "images/";
 $(document).ready(function () {
