269c27811404032642572ee2ece4ae0927958d11
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-07-30 11:10:31 -0700

    Update index.js

--- lesson04/assignment/index.js
+++ lesson04/assignment/index.js
@@ -1,14 +1,17 @@
-// JavaScript source code
-var images = ["img/vsd.jpg", "img/fdsdb.jpg", "img/MV5BMjAzNjYwNzkxMl5BMl5BanBnXkFtZTgwMTU1OTk4MDE_V1_UY317_CR16,0,214,317_AL_.jpg", "img/benjamin-parker-OhKElOkQ3R-unsplash-cropped.jpg"];
+// array storing paths to images
+var images = ["img/vsd.jpg", "img/fdsdb.jpg", "img/MV5BMjAzNjYwNzkxMl5BMl5BanBnXkFtZTgwMTU1OTk4MDE@._V1_UY317_CR16,0,214,317_AL_.jpg", "img/benjamin-parker-OhKElOkQ3R-unsplash-cropped.jpg"];
+// index of the array for image to be displayed
 var i = 0;
-var p = "images/";
 $(document).ready(function () {
+    // initially use the first image in the array
     $("#slide").attr("src", images[i]);
     $("#left").click(function () {
-        $("#slide").attr("src", images[Math.abs(i % images.length)]);
+        // When the left side button is clicked, change the image one index down
+        $("#slide").attr("src", images[Math.abs(--i % images.length)]);
 
     });
     $("#right").click(function () {
+        // when the right button is clicked, chaneg the image one index forward
         $("img").attr("src", images[Math.abs(++i % images.length)]);
 
     });
