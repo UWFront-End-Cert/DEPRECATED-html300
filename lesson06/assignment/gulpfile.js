8cd956d0aa0e9e603bc401d3f7450085632504c3
chanjessica <jchan_sea@yahoo.com>
2019-08-01 19:41:54 -0700

    comment out the minifycss

--- lesson06/assignment/gulpfile.js
+++ lesson06/assignment/gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('sass', function() {
                 browsers: ['> .5%'],
             })
         )
-        .pipe(minifycss({ compatibility: 'ie8' }))
+        // .pipe(minifycss({ compatibility: 'ie8' }))
         .pipe(gulp.dest('css/'))
         .pipe(browserSync.stream());
 });
