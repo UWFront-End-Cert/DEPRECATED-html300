e609fea31157cd962119cceb99f17f7701ffa8fa
jaamp <jt.projectclass@gmail.com>
2019-08-10 21:23:06 -0700

    adding assignment 7

---
+++ lesson07/assignment/assignment07/src/components/gridView.vue
@@ -0,0 +1,85 @@
+<template>
+<div>
+    <h2>Bootstrap Vue Grid</h2>
+    <div class="container">
+      <div class="row">
+ <!-- sets the grid row responsiveness  --> 
+        <div class="col col-xl-2 col-lg-4 col-md-6 col-12 bg-info d-flex align-items-stretch">
+ <!-- builds the card  -->          
+          <div class="card">
+            <img src="../assets/ebla.jpg" class="card-img-top" width="100" alt="...">
+            <div class="card-body d-flex align-items-center">
+              <h5 class="card-title">Ebey’s Landing National Historical Reserve</h5>
+            </div>
+            <a href="https://www.nps.gov/ebla/index.htm" class="btn btn-primary m-3">Check It Out</a>
+          </div>
+        </div>
+<!-- sets the grid row responsiveness  --> 
+        <div class="col col-xl-2 col-lg-4 col-md-6 col-12 bg-info d-flex align-items-stretch">
+ <!-- builds the card  -->          
+          <div class="card">
+            <img src="../assets/sajh.jpg" class="card-img-top" width="100" alt="...">
+            <div class="card-body d-flex align-items-center">
+              <h5 class="card-title">San Juan Island National Historical Park</h5>
+            </div>
+            <a href="https://www.nps.gov/sajh/index.htm" class="btn btn-primary m-3">Check It Out</a>
+          </div>
+        </div>
+ <!-- sets the grid row responsiveness  --> 
+        <div class="col col-xl-2 col-lg-4 col-md-6 col-12 bg-info d-flex align-items-stretch">
+ <!-- builds the card  -->          
+          <div class="card">
+            <img src="../assets/klse.jpg" class="card-img-top" width="100" alt="...">
+            <div class="card-body d-flex align-items-center">
+              <h5 class="card-title">Klondike Gold Rush - Seattle Element</h5>
+            </div>
+            <a href="https://www.nps.gov/klse/index.htm" class="btn btn-primary m-3">Check It Out</a>
+          </div>
+        </div>
+ <!-- sets the grid row responsiveness  --> 
+        <div class="col col-xl-2 col-lg-4 col-md-6 col-12 bg-info d-flex align-items-stretch">
+ <!-- builds the card  -->          
+          <div class="card">
+            <img src="../assets/mora.jpg" class="card-img-top" width="100" alt="...">
+            <div class="card-body d-flex align-items-center">
+              <h5 class="card-title">Mount Rainier National Park</h5>
+            </div>
+            <a href="https://www.nps.gov/mora/index.htm" class="btn btn-primary m-3">Check It Out</a>
+          </div>
+        </div>
+ <!-- sets the grid row responsiveness  --> 
+        <div class="col col-xl-2 col-lg-4 col-md-6 col-12 bg-info d-flex align-items-stretch">
+<!-- builds the card  -->           
+          <div class="card">
+            <img src="../assets/olym.jpg" class="card-img-top" width="100" alt="...">
+            <div class="card-body d-flex align-items-center">
+              <h5 class="card-title">Olympic National Park</h5>
+            </div>
+            <a href="https://www.nps.gov/olym/index.htm" class="btn btn-primary m-3">Check It Out</a>
+          </div>
+        </div>
+ <!-- sets the grid row responsiveness  --> 
+        <div class="col col-xl-2 col-lg-4 col-md-6 col-12 bg-info d-flex align-items-stretch">
+<!-- builds the card  --> 
+          <div class="card">
+            <img src="../assets/wing.jpg" class="card-img-top" width="100" alt="...">
+            <div class="card-body d-flex align-items-center">
+              <h5 class="card-title">Wing Luke Museum</h5>
+            </div>
+              <a href="https://www.nps.gov/wing/index.htm" class="btn btn-primary m-3">Check It Out</a>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+ name: 'gridView'   
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ lesson07/assignment/assignment07/src/components/indexView.vue
@@ -0,0 +1,55 @@
+<template>
+    <main class="main">
+    <h2>Button ToolTip and Toggle</h2>
+    <!-- builds the form  -->
+    <form id="form2">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h3 v-if="show">Welcome to the Matrix</h3>
+            </div>
+          </div>
+          <br />
+          <div class="row">
+            <div class="col-2"></div>
+            <div class="col-3">
+              <!-- buttons to show toop tip and to enable disabled button -->
+              <button v-on:click="redBtn($event)" id="red-btn" class="btn btn-danger" value="Submit" data-toggle="tooltip" title="Click the Blue Pill to Activate Me"></button>
+              <button v-on:click="blueBtn($event)" id="blue-btn" class="btn btn-primary" value="Blue Pill"></button>
+            </div>
+          </div>
+        </div>
+    </form>
+  </main>
+</template>
+<script>
+export default {
+ name: 'indexView',
+   data() {
+     return {
+    counter: 0,
+    show: false
+     }
+  },
+    methods: {
+    redBtn: function (e) {
+      e.preventDefault()
+      if(this.counter === 1) {
+      this.show = true;
+      this.counter = 0;
+      }
+    },
+    blueBtn: function (e) {
+      e.preventDefault()
+      this.counter = 1;
+      this.show = false;
+    }
+  }
+
+  }
+
+</script>
+
+<style scoped>
+
+</style>
