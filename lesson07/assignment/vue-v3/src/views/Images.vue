510a2cef9f1ea68a448f3a51ed93321376a2197e
PCS Montis <pcsmontis@outlook.com>
2019-08-19 13:49:07 -0700

    fixed code

--- lesson07/assignment/vue-v3/src/views/Images.vue
+++ lesson07/assignment/vue-v3/src/views/Images.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="images" >
-   
-<img v-for="image in images" :src="image.url" :alt="image.alt"/>
 
+    <b-img v-for="item in images" fluid :key="item.id" :src="item.image" :alt="item.alt">
+        
+    </b-img>
     
   </div>
 </template>
@@ -15,13 +16,29 @@ export default {
   
   data () {
     return {
-      
-      images: [
-        { url: '@/public/img/PANO_20190729_184301.vr.jpg', alt: 'Pano Lake Anette'},
-        { url: "./src/assets/a20180317_165049320_iOS.jpg", alt: 'GS Racing'},
-        { url: "./src/assets/a20180527_193251524_iOS.jpg", alt: 'Kendall Couloir'},
-        { url: "./src/ssets/a20180623_183319598_iOS.jpg", alt: 'GS Racing'},
+      images: [{
+        id:1,
+        image: require('@/assets/img/PANO_20190729_184301.vr.jpg'),
+        alt: 'Pano Lake Anette'
+      },
+      {
+        id:2,
+        image: require('@/assets/img/a20180317_165049320_iOS.jpg'),
+        alt: 'GS Racing'
+      },
+      {
+        id:3,
+        image: require('@/assets/img/a20180527_193251524_iOS.jpg'),
+        alt: 'Kendall Couloir'
+      },
+      {
+        id:4,
+        image: require('@/assets/img/a20180623_183319598_iOS.jpg'),
+        alt: 'Trout'
+      },
+
       ]
+
     }
   }
 }
