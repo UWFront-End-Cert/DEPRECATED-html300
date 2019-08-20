61329f5de9c2402a0e8c397c672da23f23df2425
Daniel Martinez <dcmar18@gmail.com>
2019-08-20 01:24:00 -0700

    Updated files with comments

---
+++ lesson07/assignment/router-app/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <!-- Creates header and image that will be used on all pages -->
+  <div id="app">
+    <!-- Set header text and apply background image -->
+    <header class="head text-center">
+      <h1>North Seattle Soccer League</h1>
+    </header>
+    <!-- Create and style nav elements from the nav array -->
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <div class="navbar-nav" v-for="link in nav">
+        <router-link :to="{ name: link.name }" class="nav-link">{{link.display}}</router-link>
+      </div>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  //Array of pages to create navigation
+  data () {
+    return {
+      nav: [
+        {name: 'Hello', display: 'Home'},
+        {name: 'Teams', display: 'Teams'},
+        {name: 'Rules', display: 'Rules'},
+        {name: 'Contact', display: 'Contact'}
+      ]
+    }
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+/* Set background image to cover div */
+.head {
+  background-image: url("../imgs/ball-fun-game.jpg");
+  background-color: blue;
+  height: 20rem;
+  background-size: cover;
+}
+/* Sets font size to scale depending on screen size */
+h1{
+  font-size: 5vw;
+}
+</style>
---
+++ lesson07/assignment/router-app/src/components/Contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- Uses bootstrap to create a container with 10 flex-columns -->
+    <main class="container">
+      <div class="row">
+        <div class="col">
+          <h2>Fields</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col-8">
+          <h2>Contact Us</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="col">
+          <h2>Sponsors</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Contact'
+  }
+</script>
