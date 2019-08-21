18a3e44760f8ff4a1e1041d8923ad8366349d645
ymmy1 <chem-pion@mail.ru>
2019-08-20 19:53:45 -0700

    Getting there

--- lesson08/assignment/components/AppImages.vue
+++ lesson08/assignment/components/AppImages.vue
@@ -1,9 +1,13 @@
 <template>
-		<div class="poster">
-			<img v-bind:src='link.src' v-bind:alt='link.alt' class="img-fluid img-thumbnail">
-			<p class="title">{{ link.title }}</p>
-			<button>Apply Fancy border</button>
-</div>
+	<div class="image_template">
+		<div class="poster" v-for="link in album" v-bind:key='link.id'>
+		<img v-bind:src='link.src' v-bind:alt='link.alt' class="img-fluid img-thumbnail">
+		<p>{{link.title}}</p>
+		<p>{{link.info}}</p>
+	</div>
+	</div>
+	
+
 </template>
 
 <style lang="scss" scoped>
@@ -13,15 +17,20 @@
 	flex-direction: column;
 }
 .image_template{
-    display: flex;
-    justify-content: space-around;
-    flex-flow: row wrap;
-    img{
-        width: 30%;
-        margin-bottom: 1%;
-        object-fit: cover; 
-    }
-}
+	display: flex;
+		justify-content: space-around;
+	flex-flow: row wrap;
+	.poster{
+		width: 30%;
+		margin-bottom: 1%;
+		img{
+		object-fit: cover; 
+		}
+		p{
+		color: grey;
+		font-style: italic;
+		}
+}}
 
 </style>
 
@@ -30,6 +39,18 @@
 	export default{
         
 		name: "AppImages",
-		props:['src', 'alt', 'title']
+		data () {
+            return{
+                album: [{ id: 1, title:'Beautiful Waterfall', info:'(Click on the Image to change the border)', alt:'Beautiful Waterfall', src: require('../src/assets/img-1.jpg')}, 
+                        { id: 2, title:'Deep in the Forest', info:'(Click on the Image to change the border)', alt:'Deep in the Forest', src: require('../src/assets/img-2.jpg')},
+                        { id: 3, title:'Stunning Mountains', info:'(Click on the Image to change the border)', alt:'Stunning Mountains', src: require('../src/assets/img-3.jpg')},
+                        { id: 4, title:'Lost in the Woods', info:'(Click on the Image to change the border)', alt:'Lost in the Woods', src: require('../src/assets/img-7.jpg')},
+                        { id: 5, title:'Lonely Tree', info:'(Click on the Image to change the border)', alt:'Lonely Tree', src: require('../src/assets/img-8.jpg')},
+                        { id: 6, title:'Sunset in Jungle', info:'(Click on the Image to change the border)', alt:'Sunset in Jungle', src: require('../src/assets/img-9.jpg')}
+                        ],
+                     
+
+            }
+		}
 	}
 </script>
--- lesson08/assignment/src/views/images.vue
+++ lesson08/assignment/src/views/images.vue
@@ -1,18 +1,10 @@
 <template>
   <div id="images">
     <section class=" text-center">
-      <h1 class="display-4">Nature Images</h1>
-      <div  class="image_template">        
- <section v-if="album">
-      <AppImages
-      v-for="link in album"
-      v-bind:poster='link'
-      v-bind:key='link.id'
-      >   
-      </AppImages>
+      <h1 class="display-4">Nature Images</h1>        
+
+      <AppImages />
       
-      </section>
-  </div>
 </section>
 </div>
 </template>
@@ -20,25 +12,10 @@
 
 <script>
     import AppImages from '../../components/AppImages.vue';
-    import Btn from '../../components/Btn.vue';
-    export default{
+    export default {
         name: 'images',
         components:{
-            AppImages,
-            Btn
-        },
-        data () {
-            return{
-                album: [{ id: 1, title:'test', alt:'alttest', src: require('../assets/img-1.jpg')}, 
-                        { id: 2, title:'test', alt:'alttest', src: require('../assets/img-2.jpg')},
-                        { id: 3, title:'test', alt:'alttest', src: require('../assets/img-3.jpg')},
-                        { id: 4, title:'test', alt:'alttest', src: require('../assets/img-7.jpg')},
-                        { id: 5, title:'test', alt:'alttest', src: require('../assets/img-8.jpg')},
-                        { id: 6, title:'test', alt:'alttest', src: require('../assets/img-9.jpg')}
-                        ]
-
-            }
-            
+            AppImages
         }
-    }
+      }
 </script>
