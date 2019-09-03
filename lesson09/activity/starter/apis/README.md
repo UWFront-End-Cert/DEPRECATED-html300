6c1ce3718f47d9b201bb9bf2f610da2e23a90100
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-09-03 00:41:54 -0700

    Add files via upload

--- lesson09/activity/starter/apis/README.md
+++ lesson09/activity/starter/apis/README.md
@@ -1,34 +1,22 @@
-# API Activity
+# JIR
 
-## Instructions
-* Make sure `yarn` is installed, check [here](https://yarnpkg.com/lang/en/docs/install/).
-* Verify by running `yarn -v` in your terminal to see if a version comes back
-* Run `yarn` to install, verify packages installed successfully
-* Add Axios when prompted
-* Run `yarn run dev` to launch the local server
-* Use Axios to fetch data from the [restcountries.eu](https://restcountries.eu/#api-endpoints-region) endpoint
-* Create a v-for loop to print each country into a newly created component, Card
-* On each country's 'Card' component, include the flag image/alt text, country name, and subregion. 
-
-
-> Nuxt.js project
+> My swell Nuxt.js project
 
 ## Build Setup
 
 ``` bash
 # install dependencies
-$ npm install # Or yarn install
+$ npm run install
 
 # serve with hot reload at localhost:3000
 $ npm run dev
 
 # build for production and launch server
 $ npm run build
-$ npm start
+$ npm run start
 
 # generate static project
 $ npm run generate
 ```
 
-For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
-
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
--- lesson09/activity/starter/apis/nuxt.config.js
+++ lesson09/activity/starter/apis/nuxt.config.js
@@ -1,41 +1,63 @@
-module.exports = {
-  modules: [
-    '@nuxtjs/axios',
-  ],
+export default {
+  mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'Vue API & Routes',
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
   /*
-  ** Build configuration
-  */
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org/docs/
+    'bootstrap-vue/nuxt',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '~/assets/generateCalendar.js'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
