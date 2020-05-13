00cc4a6dd3aa7cd79b4f5d7a0c3ceb5a0e9e8749
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-05-13 10:35:17 -0500

    began building other examples of ES6+jQuery in Assignment01

---
+++ courseProject/PROPOSAL.md
@@ -0,0 +1,25 @@
+# Topic: Merissa O'Reilly
+
+* Nav
+  * Home [link](https://.com)
+  * Projects [link](https://.com)
+  * Skills [link](https://.com)
+  * About Me [link](https://.com)
+
+## Why I Chose This Subject
+  I want to build this project to serve as a portfolio that showcases my technical skills, while including my own personal brand that highlights my soft skills, what I would offer as a team member, and my past successes that incorporate how I would translate my old skills into new.
+
+### API
+The API I plan to include is Goodreads. Personally, I am an avid reader. Including this API will allow users to search for books to read easily.
+Goodreads [link](https://www.goodreads.com/api)
+
+-------------
+
+Some items to include in the project moving forward:
+
+Personal Vision Statement
+To acheive success as a front-end web developer, working well in a team, inspiring others, and contributing to my own community is important. I will do this by drawing on past experiences in executing a week-long fully fucntioning guest schedule for a Masters Week(R) guest house to be used by five team members; spearheading the inaugural Sustainable Hospitality Development Symposium on an accelerated timeline [link](https://csumb.edu/hospitality/sustainable-hospitality-development-symposium); and inspiring my community as a board member for the Augusta Handmade Fair and SFRG Treasurer.
+Accordingly, I will focus on continous professional learning and growth.
+
+About Me
+-volunteered as AHF board member, SFRG treasurer (walking tour)
--- lesson01/assignments/scripts.js
+++ lesson01/assignments/scripts.js
@@ -1,26 +1,51 @@
-// jQuery Doc Ready
-$(function () {
-    // Set up some data and variables
-    const data = [1, 2, 3, 4],
-        input = $('.input'),
-        submit = $('.submit'),
-        resultSum = $('.sum');
-    // Click event on the submit input
-    submit.on('click', function () {
-        // Grab the input value and parse it into a number
-        const val = input.val();
-        const num = parseInt(val);
-        // Verify the user gave us a real number
-        if (!isNaN(num)) {
-            // Add our new number to the data array
-            data.push(num);
-            // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
-            const sum = data.reduce((acc, curr) => acc + curr);
-            // Add our new total to the page
-            resultSum.text(sum);
-            // Reset the input value to an empty string
-            input.val('');
-        }
-    });
+// // jQuery Doc Ready
+$(function() {
+  const data = [1, 2, 3, 4],
+    input = $('.input'),
+    submit = $('.submit'),
+    resultSum = $('.sum');
+  // Click event on the submit input
+  submit.on('click', function() {
+    // Grab the input value and parse it into a number
+    const val = input.val();
+    const num = parseInt(val);
+    // Verify the user gave us a real number
+    if (!isNaN(num)) {
+      // Add our new number to the data array
+      data.push(num);
+      // Use reduce to convert our array of numbers into a single value - the sum of all of the numbers
+      const sum = data.reduce((acc, curr) => acc + curr);
+      // Add our new total to the page
+      resultSum.text(sum);
+      // Reset the input value to an empty string
+      input.val('');
+    }
+  });
+
+
+  let dataThree = $('.inputTwo');
+    inputTwo = $('.inputTwo'),
+    submitTwo = $('.submitTwo'),
+    resultWord = $('.word');
+
+  submitTwo.on('click', function() {
+    resultWord.text(`What does the ${dataThree.val()} say?`);
+  });
 });
 
+  // let dataThree = [9, 10, 11, 12];
+  // let found =
+//
+// The goal of this assignment is to use ES6+ with a  front-end framework, and you already know how to use one, jQuery.
+//
+// Create at least sets of 4 input boxes. Each box should have a button the user can press.
+// Use jQuery and ES6+ to manipulate the data from those input boxes and ouput the result withan explantation.
+// Use at least 4 ES6+ methods.
+//
+// Remember it's best practice to add comments to your code.
+// Don't worry about validating data. Only number will be added in the input box.
+//
+// Optional Bonus: If you have completed the requirements above and are up for an extra challenge, you can take your project to the next level with following bonus activities.
+//
+// Use string template literals for the output and use arrow functions.
+// Integrate two more ES6+ functions.
