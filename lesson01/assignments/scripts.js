6ea2354e0b7305081fb919b0eca77dfe2580ee74
Heather Malloy <hmalloy@bluetooth.com>
2020-05-18 15:29:52 -0700

    cleaned up code and added comments

--- lesson01/assignments/scripts.js
+++ lesson01/assignments/scripts.js
@@ -4,127 +4,167 @@ $(function () {
       array = $('.array');
       input = $('.input'),
       btn = input.next(),
-      result = btn.next(),
+      result = btn.next();
+
+      // set the initial array at the top of the page
       initial.forEach(function(value, key){
         array.append(`# ${key} `);
         array.append(`the value is ${value}<br>`);
       });
 
       // map
-        // will clear the new array if user goes to enter a new number
-      $('.map .input').on('keyup', function(){
-        $('.map .markup').text(' ');
+      let mapDiv = $('.map');
+      // clear the result if enter a new number
+      mapDiv.find(input).on('keyup', function(){
+        mapDiv.find(result).text(' ');
       });
-        // takes user input and adds it to each # in the array,
+        // adds user input to each # in the array
         // creates a new array
-      $('.map .submit').on('click', function(){
-        let val = $('.map .input').val();
+      mapDiv.find(btn).on('click', function(){
+        let val = input.val();
         let mapVal = parseInt(val);
-        let mapArray = initial.map(value => value + mapVal);
 
-        console.log(mapArray);
+        //make sure entry is a number
+        if (!isNaN(mapVal)) {
+          let mapArray = initial.map(value => value + mapVal);
 
-        mapArray.forEach(function(value, key){
-          $('.map .markup').append(`# ${key} `);
-          $('.map .markup').append(`the <strong>new</strong> value is ${value}<br>`);
-        });
-        input.val('');
+          // displays a new array
+          mapArray.forEach(function(value, key){
+            mapDiv.find(result).append(`# ${key} `);
+            mapDiv.find(result).append(`the <strong>new</strong> value is ${value}<br>`);
+          });
+          // clears input
+          input.val('');
+        }
       });
 
       // filter
-        // will clear the new array if user goes to enter a new number
-      $('.filter .input').on('keyup', function(){
-        $('.filter .markup').text(' ');
+      let filterDiv = $('.filter');
+      // clear the result if enter a new number
+      filterDiv.find(input).on('keyup', function(){
+        filterDiv.find(result).text(' ');
       });
-        // takes user input and filters the array to any number greater than user input,
+        // filter array to any number greater than user input,
         // creates a new array
-      $('.filter .submit').on('click', function(){
-        let val = $('.filter .input').val();
+      filterDiv.find(btn).on('click', function(){
+        let val = filterDiv.find(input).val();
         let filterVal = parseInt(val);
-        let filterArray = initial.filter(value => value > filterVal);
 
-        console.log(filterArray);
+        //make sure entry is a number
+        if (!isNaN(filterVal)) {
+          // filter all numbers greater than user input
+          let filterArray = initial.filter(value => value > filterVal);
 
-        filterArray.forEach(function(value, key){
-          // $('.filter .markup').append(`# ${key} `);
-          $('.filter .markup').append(`The #${key + 1} value greater than ${filterVal} is ${value}<br>`);
-        });
-        input.val('');
+          filterArray.forEach(function(value, key){
+            filterDiv.find(result).append(`The #${key + 1} value greater than ${filterVal} is ${value}<br>`);
+          });
+          // clear input
+          input.val('');
+        }
       });
 
       // reduce
-      $('.reduce .submit').on('click', function(){
-        let val = $('.reduce .input').val();
+      let reduceDiv = $('.reduce');
+
+      reduceDiv.find(btn).on('click', function(){
+        let val = reduceDiv.find(input).val();
         let reduceVal = parseInt(val);
-        let arrayLength = initial.length;
-        initial.push(reduceVal);
-        let reduceSum = initial.reduce((sum, value) => sum + value);
 
-        // update the array list at top with the added value(s)
-        array.append(`# ${arrayLength} `);
-        array.append(`the value is ${reduceVal}<br>`);
+        //make sure entry is a number
+        if (!isNaN(reduceVal)) {
+          // capture length of array for new objects
+          let arrayLength = initial.length;
+          // add new number to existing array
+          initial.push(reduceVal);
+          // get the sum of the array with the new number(s) included
+          let reduceSum = initial.reduce((sum, value) => sum + value);
 
-        $('.reduce .markup').text(`The sum of the array plus your addition of the number ${reduceVal} is ${reduceSum}.`);
+          // update the array list at top with the added value(s)
+          array.append(`# ${arrayLength} `);
+          array.append(`the value is ${reduceVal}<br>`);
 
-        input.val('');
+          // display results
+          reduceDiv.find(result).text(`The sum of the array plus your addition of the number ${reduceVal} is ${reduceSum}.`);
+          // clear input value
+          input.val('');
+        }
       });
 
       // find
-        // will clear the new array if user goes to enter a new number
-      $('.find .input').on('keyup', function(){
-        $('.find .markup').text(' ');
+      let findDiv = $('.find');
+      // clear the result if enter a new number
+      findDiv.find(input).on('keyup', function(){
+        findDiv.find(result).text(' ');
       });
-        // takes user input and finds the next number in the array greater than user input
-      $('.find .submit').on('click', function(){
-        let val = $('.find .input').val();
+        // finds the next number in the array greater than user input
+      findDiv.find(btn).on('click', function(){
+        let val = findDiv.find(input).val();
         let findVal = parseInt(val);
-        let found = initial.find(value => value > findVal);
 
-        $('.find .markup').text(`I have found, for you, the number ${found}! This is the first number in the array that is over ${findVal}.`);
+        //make sure entry is a number
+        if (!isNaN(findVal)) {
+          // find first number greater than user input
+          let found = initial.find(value => value > findVal);
 
-        input.val('');
+          // display results
+          findDiv.find(result).text(`I have found, for you, the number ${found}! This is the first number in the array that is over ${findVal}.`);
+
+          // clear input
+          input.val('');
+        }
       });
 
       // some
-        // will clear the new array if user goes to enter a new number
-      $('.some .input').on('keyup', function(){
-        $('.some .markup').text(' ');
+      let someDiv = $('.some');
+      // clear the result if enter a new number
+      someDiv.find(input).on('keyup', function(){
+        someDiv.find(result).text(' ');
       });
         // takes user input and finds the next number in the array greater than user input
-      $('.some .submit').on('click', function(){
-        let val = $('.some .input').val();
+      someDiv.find(btn).on('click', function(){
+        let val = someDiv.find(input).val();
         let someVal = parseInt(val);
-        let someArray = initial.filter(value => value < someVal);
 
-        console.log(someArray);
+        //make sure entry is a number
+        if (!isNaN(someVal)) {
+          // get values less than user input
+          let someArray = initial.filter(value => value < someVal);
 
-        someArray.forEach(function(value, key){
-          // $('.filter .markup').append(`# ${key} `);
-          $('.some .markup').append(`The #${key + 1} value less than ${someVal} is ${value}<br>`);
-        });
-        input.val('');
+          // display results
+          someArray.forEach(function(value, key){
+            someDiv.find(result).append(`The #${key + 1} value less than ${someVal} is ${value}<br>`);
+          });
+          // clear input
+          input.val('');
+        }
       });
 
       // every
+      let everyDiv = $('.every');
         // will clear the new array if user goes to enter a new number
-      $('.every .input').on('keyup', function(){
-        $('.every .markup').text(' ');
+      everyDiv.find(input).on('keyup', function(){
+        everyDiv.find(result).text(' ');
       });
         // takes user input and finds the next number in the array greater than user input
-      $('.every .submit').on('click', function(){
-        let val = $('.every .input').val();
+      everyDiv.find(btn).on('click', function(){
+        let val = everyDiv.find(input).val();
         let everyVal = parseInt(val);
-        let everyNum = initial.every(value => value < everyVal);
 
-        console.log(everyNum);
+        //make sure entry is a number
+        if (!isNaN(everyVal)) {
+          // check every number in array
+          let everyNum = initial.every(value => value < everyVal);
 
-        if(everyNum) {
-          $('.every .markup').text(`Congrats, ${everyVal} is greater than every other number in the array!`);
-        } else {
-          $('.every .markup').text(`Sorry, ${everyVal} is NOT greater than all numbers in the array!`);
+          // display result based whether or not everyNum is true
+          if(everyNum) {
+            everyDiv.find(result).text(`Congrats, ${everyVal} is greater than every other number in the array!`);
+          } else {
+            everyDiv.find(result).text(`Sorry, ${everyVal} is NOT greater than all numbers in the array!`);
+          }
+
+          // clear input
+          input.val('');
         }
-
-        input.val('');
       });
 
 });
