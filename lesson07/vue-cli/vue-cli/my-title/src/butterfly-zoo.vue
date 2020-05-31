76a8785f96e4319638cabcd6c6e93fbb2660f8e4
JennH <marlowmayfield@hotmail.com>
2020-05-31 11:55:12 -0700

    made changes

--- lesson07/vue-cli/vue-cli/my-title/src/butterfly-zoo.vue
+++ lesson07/vue-cli/vue-cli/my-title/src/butterfly-zoo.vue
@@ -61,18 +61,7 @@
     body {
       margin: 0px;
     }
-    </style>
-
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    
 h1, h2 {
   font-weight: normal;
 }
