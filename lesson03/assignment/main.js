387ff63c98ac356b9a4d23b272de0e7afc7e8803
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-06-04 15:23:40 -0500

    trouble shooting .append()

--- lesson03/assignment/main.js
+++ lesson03/assignment/main.js
@@ -1,4 +1,17 @@
+//jQuery doc ready
+
 $(function() {
+
+//Constructor function used because there are 3+ objects. NEED TO SWITCH TO ES6+
+  function Card(company, experience, school, major, email, linkedInUrl, codeLanguages) {
+    this.company = company;
+    this.experience = experience;
+    this. school = school;
+    this.major = major;
+    this.email = email;
+    this.linkedInUrl = linkedInUrl;
+    this.codeLanguages = codeLanguages;
+  }
 
 const cards = [
   {
@@ -55,9 +68,10 @@ const cards = [
   }
 ];
 
+
+//i want to append this whole SECTION to appear after the first card i hard coded. i want to loop over the objects with a method, use map() to apply a template string, would i use JSON.stringify? do i need a for loop or do i need to use for...in since it is specific for objects? The example on slide 19 is not showing an array of objects, just one object.
 const cardsHTML = cards.map(function(el) {
-  let card = `
-  <section class="card-container">
+  let card = `<section class="card-container">
     <ul>
       <li class="card_company">${el.company}</li>
       <li class="card_experience">${el.experience}</li>
@@ -70,6 +84,6 @@ const cardsHTML = cards.map(function(el) {
   </section>
   `;
     });
-$(".cards").append(cardsHTML);
+$(".s").append(card);
 
 });
