4726131af1a5972e471f0135889ab376f289d690
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-06-05 11:57:47 -0500

    renamed files; debugged main.js

--- lesson03/assignment/main.js
+++ lesson03/assignment/main.js
@@ -3,15 +3,17 @@
 $(function() {
 
 //Constructor function used because there are 3+ objects. NEED TO SWITCH TO ES6+
-  function Card(company, experience, school, major, email, linkedInUrl, codeLanguages) {
-    this.company = company;
-    this.experience = experience;
-    this. school = school;
-    this.major = major;
-    this.email = email;
-    this.linkedInUrl = linkedInUrl;
-    this.codeLanguages = codeLanguages;
-  }
+  // function Card(company, experience, school, major, email, linkedInUrl, codeLanguages) {
+  //   this.company = company;
+  //   this.experience = experience;
+  //   this. school = school;
+  //   this.major = major;
+  //   this.email = email;
+  //   this.linkedInUrl = linkedInUrl;
+  //   this.codeLanguages = codeLanguages;
+  // }
+
+
 
 const cards = [
   {
@@ -68,10 +70,10 @@ const cards = [
   }
 ];
 
-
-//i want to append this whole SECTION to appear after the first card i hard coded. i want to loop over the objects with a method, use map() to apply a template string, would i use JSON.stringify? do i need a for loop or do i need to use for...in since it is specific for objects? The example on slide 19 is not showing an array of objects, just one object.
-const cardsHTML = cards.map(function(el) {
-  let card = `<section class="card-container">
+//i want to append this whole SECTION to appear after the first card i hard coded. It is no longer throwing errors, but it is also not showing any events. WHY. Do i need to use create element? Am I  missing $?
+//i want to loop over the objects with a method, use map() to apply a template string, would i use JSON.stringify? do i need a for loop or do i need to use for...in since it is specific for objects? The example on slide 19 is not showing an array of objects, just one object.
+let cardsHTML = cards.map(function(el) {
+  return `<section class="card-container">
     <ul>
       <li class="card_company">${el.company}</li>
       <li class="card_experience">${el.experience}</li>
@@ -83,7 +85,34 @@ const cardsHTML = cards.map(function(el) {
     </ul>
   </section>
   `;
+
+//REPLACE ABOVE CODE WITH THIS CODE
+  //<section class="profile">
+  //   <ul>
+  //     <li class="card_company">
+  //       <p><span class="bold">Company:</span> Front End Dev Co</p>
+  //     </li>
+  //     <li class="card_experience">
+  //       <p><span class="bold">Experience:</span> 3 years</p>
+  //     </li>
+  //     <li class="card_school">
+  //       <p><span class="bold">School:</span> UW</p>
+  //     </li>
+  //     <li class="card_major">
+  //       <p><span class="bold">Major:</span> Marketing</p>
+  //     </li>
+  //     <li class="card_email">
+  //       <p><span class="bold">Email:</span> steve@fedc.com</p>
+  //     </li>
+  //     <li class="card_linkedInUrl"><i class="fab fa-linkedin"></i> <span class="linked-in">steve.linkedinprofile.com</span></li>
+  //     <li class="card_codeLanguages">
+  //       <p><span class="bold">Code Languages:</span> HTML, CSS, JavaScript, .NET, C#</p>
+  //     </li>
+  //   </ul>
+  // </section>
     });
-$(".s").append(card);
+card = document.createElement('card');
+$(".card-container").append(cardsHTML);
+
 
 });
