12848b9dd1a375744af5c69854f420fd7a36de8d
JennH <marlowmayfield@hotmail.com>
2020-06-07 08:32:19 -0700

    made changes and added card.vue file

---
+++ lesson09/assignment/butterfly/components/card.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+<!--  <article class="card">
+
+    <h2 class="card_title">{{insect.nameString}}</h2>
+    <p class="card_copy">{{insect.status}}</p>
+
+  </article>-->
+
+</template>
+
+<script>
+//import default {
+//  props: {
+//    insect: Object
+//  }
+//}
+//export default {
+//}
+</script>
+
+<style lang="css" scoped>
+</style>
