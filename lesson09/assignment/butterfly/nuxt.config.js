12f0cc2a28fd10f88211887dba299763154e7e74
JennH <marlowmayfield@hotmail.com>
2020-06-09 10:42:07 -0700

    made changes to nuxt.config.js for meta stuff

--- lesson09/assignment/butterfly/nuxt.config.js
+++ lesson09/assignment/butterfly/nuxt.config.js
@@ -1,20 +1,30 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'butterfly',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    //  { rel: 'site', href: 'https://www.si.edu/spotlight/buginfo/butterfly'}
-    ]
-  },
-
+  <template>
+  <h1> {{title}} </h1>
+  </template>
+  <script>
+  export default {
+    data() {
+      return {
+        title: 'World of Butterflies',
+        }
+      },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'A butterfly site for the entertainment and education of the butterfly enthusiast and lover of natural beauty.' }
+      ],
+      link: [
+      //  { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'site', href: 'https://www.si.edu/spotlight/buginfo/butterfly'}
+      ]
+    }
+  }
+}
+</script>
   /*
   ** Customize the progress bar color
   */
