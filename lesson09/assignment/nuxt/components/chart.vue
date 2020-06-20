5aac869bcb4cd931ed20504155b07993c936ab20
Nichole Godwin <headiehg@mail.com>
2020-06-20 10:06:35 -0700

    chart imperial-metric rerenders

--- lesson09/assignment/nuxt/components/chart.vue
+++ lesson09/assignment/nuxt/components/chart.vue
@@ -5,8 +5,6 @@
     <button @click='changeUnit(false)'> Metric (mm | °C) </button>
     <button @click='changeUnit(true)'>Imperial (in | °F) </button>
   </div>
-  <!-- /filter --
-	 <!--<p class="chart__copy">{{ country}}</p><br>-->
   <br>
   <highcharts :options="chartOptions" :highcharts="hcInstance"></highcharts>
 </article>
@@ -26,55 +24,63 @@ export default {
   props: {
     country: Array
   },
-
   methods: {
 
     changeUnit(e) {
       if (e == true) {
         console.log('change-t')
         console.log(e);
-        //var prcpListN = this.prcpList.map(function(element) {
-        //  return element / 25.4
-        //});
-        //var tavgListN = this.tavgList.map(function(element) {
-        //  return (element * (9 / 5)) + 32
-        //});
-        //console.log(tavgListN);
+        let prcpListN = this.prcpList.map(function(element) {
+          return element / 25.4
+        });
+        let tavgListN = this.tavgList.map(function(element) {
+          return (element * (9 / 5)) + 32
+        });
+
+        this.chartOptions.series[0]['data'] = prcpListN
+        this.chartOptions.series[1]['data'] = tavgListN
+
       } else if (e == false) {
         console.log('change-f')
         console.log(e);
-        //let prcpListN = this.prcpList
-        //let tavgListN = this.tavgList
-        //console.log(tavgListN);
+        let prcpListN = this.prcpList
+        let tavgListN = this.tavgList
+        this.chartOptions.series[0]['data'] = prcpListN
+        this.chartOptions.series[1]['data'] = tavgListN
       }
     }
 
   },
 
   data() {
-    let c = this.country;
-    var monthList = [];
-    var tavgList = [];
-    var prcpList = [];
+
+    let c = this.country
+    let monthList = []
+    let tavgList = []
+    let prcpList = []
+    let tavgListN = []
+    let prcpListN = []
 
     c.forEach(function(obj) {
       tavgList.push(obj['tavg']);
+      tavgListN.push(obj['tavg']);
     })
     c.forEach(function(obj) {
       prcpList.push(obj['prcp']);
+      prcpListN.push(obj['prcp']);
     })
     c.forEach(function(obj) {
       monthList.push(obj['month']);
     })
-    console.log('raws');
-    console.log(monthList);
-    console.log(tavgList);
-    console.log(prcpList);
 
 
     return {
-      hcInstance: Highcharts,
 
+      c: c,
+      monthList: monthList,
+      tavgList: tavgListN,
+      prcpList: prcpListN,
+      hcInstance: Highcharts,
       chartOptions: {
         title: {
           text: ''
@@ -91,8 +97,6 @@ export default {
         xAxis: {
           categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         }
-
-
       }
     }
   }
@@ -107,10 +111,8 @@ export default {
     padding-left: 1.5rem;
     padding-top: 0;
     padding-bottom: 4em;
-    //size:3em;
 }
 .chart {
-    //margin: 1rem;
     border: none; //1px solid #000;
     padding: 1.5rem;
     width: 100%;
