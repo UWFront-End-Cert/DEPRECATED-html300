60088a31c758b1a49aa4dae1be4fa2c2c08f988c
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-08-14 11:08:23 -0500

    changed file path for all assets in images.vue

--- lesson07/assignment/my-title/src/components/images.vue
+++ lesson07/assignment/my-title/src/components/images.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="display-1">{{ header }}</h1>
-    <img v-for="image in images" class="img" src="{{ imageOne.src }}" title="{{ imageOne.caption}}" alt="{{ imageOne.alt }}"/>
+    <img v-for="imageOne in images" class="img" :src="imageOne.src" :title="imageOne.caption" :alt="imageOne.alt"/>
 
   </div>
 </template>
@@ -14,26 +14,26 @@ export default {
 
         images: [
           {
-          imageOne: 'Cross Stitch',
-          src: './src/images/xstitch.jpg',
+          img: 'Cross Stitch',
+          src: './../images/xstitch.jpg',
           caption: 'Cross Stitch Flat Lay',
           alt: 'Flat lay image of cross stitch, Kindle ereader, cup of tea'
         },
         {
-          imageTwo: 'Lions',
-          src: './src/images/lions.jpg',
+          img: 'Lions',
+          src: './../images/lions.jpg',
           caption: 'Crocheted Lions',
           alt: 'Two small crocheted lions'
         },
         {
-          imageThree: 'Rainbow',
-          src: './src/images/rainbow.jpg',
+          img: 'Rainbow',
+          src: './../images/rainbow.jpg',
           caption: 'Crocheted Rainbow',
           alt: 'Crocheted rainbow'
         },
         {
-          imageFour: 'Close Up Knit',
-          src: './src/images/knit.jpg',
+          img: 'Close Up Knit',
+          src: './../images/knit.jpg',
           caption: 'Moss Stitch Close Up',
           alt: 'Close up view of changing yarn colors in knit'
         }
