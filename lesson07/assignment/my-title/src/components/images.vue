c74d901336f400ac054bf3e37d0d5e890fa44413
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-08-18 17:52:16 -0500

    added function to display dynamic images

--- lesson07/assignment/my-title/src/components/images.vue
+++ lesson07/assignment/my-title/src/components/images.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1 class="display-1">{{ header }}</h1>
-    <div class="col-lg-2" v-for="imageOne in images">
-      <img v-for="imageOne in images" class="img" :src="getPic" :title="imageOne.caption" :alt="imageOne.alt"/>
+    <div class="col-md-2">
+      <img v-for="imageOne in images" class="img" :src="getPic(imageOne.src)" :title="imageOne.caption" :alt="imageOne.alt"/>
     </div>
 
   </div>
@@ -17,38 +17,45 @@ export default {
         images: [
           {
           img: 'Cross Stitch',
-          src: './../images/xstitch.jpg',
+          src: 'xstitch.jpg',
           caption: 'Cross Stitch Flat Lay',
           alt: 'Flat lay image of cross stitch, Kindle ereader, cup of tea'
         },
         {
           img: 'Lions',
-          src: './../images/lions.jpg',
+          src: 'lions.jpg',
           caption: 'Crocheted Lions',
           alt: 'Two small crocheted lions'
         },
         {
           img: 'Rainbow',
-          src: './../images/rainbow.jpg',
+          src: 'rainbow.jpg',
           caption: 'Crocheted Rainbow',
           alt: 'Crocheted rainbow'
         },
         {
           img: 'Close Up Knit',
-          src: './../images/knit.jpg',
+          src: 'knit.jpg',
           caption: 'Moss Stitch Close Up',
           alt: 'Close up view of changing yarn colors in knit'
         }
       ]
-    },
+    }
+  },
   methods: {
-    getPic() {
-      return './../images/' + this.src;
+    getPic(pic) {
+      return require(`@/images/${pic}`)
       }
     }
   //https://medium.com/@renatello/how-to-bind-img-src-in-vue-js-5690ac89c1bb
 
-    }
   }
 
 </script>
+
+<style>
+  img {
+    height: auto;
+    width: 33%;
+  }
+</style>
