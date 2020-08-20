e89760a476473b32a43db1a153236e538f1eeed5
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-08-20 15:34:56 -0500

    working on fucntion for accordion.vue

--- lesson07/assignment/my-title/src/components/grid.vue
+++ lesson07/assignment/my-title/src/components/grid.vue
@@ -1,8 +1,14 @@
 <template>
   <div >
     <h1 class="display-1">{{ header }}</h1>
+    <!-- want to iterate through wach object in gridData to populate grid -->
     <div id="grid-container">
-      <div class="grid-item"></div>
+      <div class="grid-item">
+        <h2>{{ subhead }}</h2>
+        <p>Changing Content</p>
+        <!--will use button to change content of grid with v-if-else-->
+        <button></button>
+      </div>
     </div>
   </div>
 </template>
@@ -13,31 +19,32 @@ export default {
     return {
       header: 'Grid'
       // "v-if v-else" statement to change content or UI.
-    },
+    }
     gridData: [
       {
-        h2: 'Blurb One',
+        subhead: 'Blurb One',
         p: 'Roquefort say cheese when the cheese comes out everybodys happy.'
       },
       {
-        h2: 'Blurb Two',
+        subhead: 'Blurb Two',
         p: 'Camembert de normandie gouda brie ricotta taleggio bavarian bergkase swiss boursin.'
       },
       {
-        h2: 'Blurb Three',
+        subhead: 'Blurb Three',
         p: 'Bocconcini pepper jack cow macaroni cheese queso croque monsieur edam emmental.'
       },
       {
-      h2: 'Blurb Four',
+      subhead: 'Blurb Four',
       p: 'Squirty cheese manchego monterey jack fromage.'
       },
       {
-        h2: 'Blurb Five',
+        subhead: 'Blurb Five',
         p: 'Hard cheese brie say cheese.'
       }
     ]
-  }
-}
+      }
+    }
+
 </script>
 
 <style>
